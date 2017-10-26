c34fc32215126c0c371bc59f408c032d91bf7ad1
Dave Seah <david@davidseah.com>
2017-10-25 23:24:55 -0400

    reorganize directory structure. package.json is now in root, code modules in system. see wiki for details.

--- system/app/main.js
+++ system/app/main.js
@@ -2,26 +2,23 @@
 
 	Thoughtometa is a experimental code-driven knowledge authoring tool.
 
+	Concept: create a module that allows us to define chunks of data that
+	can be tagged.
+
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -~v~- ///////////////////////////////////*/
 
 /** LIBRARIES ****************************************************************/
 
+	// definer module manages chunks of data
 	const DEF = require('../modules/definer');
 
 /** RUNTIME ******************************************************************/
 
 	console.log("SUPER DUPER THOUGHTOMETA 1.0");
 
-	DEF.Section('header',"This is my header");
 
-	// start making up syntax for defining a semantic chunk
-	// DEF.Section('bananas');
-	// DEF.AddContent('Bananas are yellow');
-
-	// create HTML?
-	// DEF.SortBy('property');
-	// DEF.HTMLOut();
-
-	// make node stick around so the browser debugger has a chance to startup 
-	// use control-c to stop
+///////////////////////////////////////////////////////////////////////////////
+/** MODULE WAIT **************************************************************/
+/// make node stick around so the browser debugger has a chance to startup 
+//  use control-c to stop
 	(function wait () { setTimeout(wait, 1000); })();
